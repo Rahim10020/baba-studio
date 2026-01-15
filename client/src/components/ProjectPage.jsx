3d8d9005f12ada9605565a36ff7101a386744e88
Rahim10020 <rahialighi@gmail.com>
2026-01-15 14:37:37 +0000

    correction souci

--- client/src/components/ProjectPage.jsx
+++ client/src/components/ProjectPage.jsx
@@ -20,13 +20,15 @@ const useScrollAnimation = () => {
       { threshold: 0.1, rootMargin: "0px 0px -50px 0px" }
     );
 
-    if (ref.current) {
-      observer.observe(ref.current);
+    const currentRef = ref.current;
+
+    if (currentRef) {
+      observer.observe(currentRef);
     }
 
     return () => {
-      if (ref.current) {
-        observer.unobserve(ref.current);
+      if (currentRef) {
+        observer.unobserve(currentRef);
       }
     };
   }, []);
